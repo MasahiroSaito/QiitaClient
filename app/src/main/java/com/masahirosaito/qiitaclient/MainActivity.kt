77ac671233d77d0049e9bf4a677e15fc30ffb453
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-04-15 20:56:16 +0900

    Update MainActivity

--- app/src/main/java/com/masahirosaito/qiitaclient/MainActivity.kt
+++ app/src/main/java/com/masahirosaito/qiitaclient/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.masahirosaito.qiitaclient
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.masahirosaito.qiitaclient.model.Article
+import com.masahirosaito.qiitaclient.model.User
+import com.masahirosaito.qiitaclient.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // ArticleViewオブジェクトを生成
+        val articleView = ArticleView(applicationContext)
+
+        // Articleオブジェクトを生成して、ArticleViewオブジェクトにセット
+
+        articleView.setArticle(Article(
+                id = "123",
+                title = "Kotlin入門",
+                url = "http://www.example.com/articles/123",
+                user = User(
+                        id = "456",
+                        name = "たろう",
+                        profileImageUrl = "")))
+
+        setContentView(articleView)
     }
 }
