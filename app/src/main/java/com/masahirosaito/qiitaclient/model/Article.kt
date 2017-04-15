3d74b0da0e59a259752812be76efd529b05b8306
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-04-15 18:41:25 +0900

    Create Article data class

---
+++ app/src/main/java/com/masahirosaito/qiitaclient/model/Article.kt
@@ -0,0 +1,6 @@
+package com.masahirosaito.qiitaclient.model
+
+data class Article(val id: String,
+                   val title: String,
+                   val url: String,
+                   val user: User)
