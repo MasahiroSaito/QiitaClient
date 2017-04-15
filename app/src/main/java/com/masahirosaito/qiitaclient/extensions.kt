c66f474cb5c510adc0e37aad6d8d6f8b80f5108c
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-04-15 20:56:49 +0900

    Create View.bindView extension method

---
+++ app/src/main/java/com/masahirosaito/qiitaclient/extensions.kt
@@ -0,0 +1,10 @@
+@file:Suppress("UNCHECKED_CAST")
+
+package com.masahirosaito.qiitaclient
+
+import android.support.annotation.IdRes
+import android.view.View
+
+fun <T : View> View.bindView(@IdRes id: Int): Lazy<T> = lazy {
+    findViewById(id) as T
+}
